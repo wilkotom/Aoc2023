c493274a8d6b4e9cc2ede2769e464a360c5a93b5
Tom Wilkinson <tom.github@dentrasssi.net>
2023-12-08 08:07:29 +0000

    Ghosts in a desert. Pac Man writ large?

---
+++ day08/src/main.rs
@@ -0,0 +1,81 @@
+use std::{error::Error, collections::HashMap};
+use aochelpers::{get_daily_input,lcm};
+
+#[derive(Debug,PartialEq, Eq)]
+struct NextStage  {
+    right: String,
+    left: String
+}
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let (directions, map) = parse_data(&get_daily_input(8,2023)?);
+    println!("Part 1: {}", follow_directions(&directions, &map, "AAA"));
+    println!("Part 2: {}", direct_ghosts(&directions, &map));
+    Ok(())
+}
+
+fn follow_directions(directions: &str, map: &HashMap<String, NextStage>, start: &str) -> usize {
+    let mut steps = 0;
+    let mut current_location = start;
+    while !current_location.ends_with('Z') {
+        if directions.chars().nth(steps % directions.len()).unwrap() == 'R' {
+            current_location = &map.get(current_location).unwrap().right;
+        } else {
+            current_location = &map.get(current_location).unwrap().left;
+        }
+        steps += 1
+    }
+    steps
+}
+
+fn direct_ghosts(directions: &str, map: &HashMap<String, NextStage>) -> usize {
+    map.keys().filter(|x| x.ends_with('A')).map(|x| follow_directions(directions, map, x)).reduce(lcm).unwrap()
+}
+
+fn parse_data(data: &str) -> (String, HashMap<String, NextStage>) {
+    let mut sections = data.split("\n\n");
+    let steps = sections.next().unwrap().to_string();
+    let mut map = HashMap::new();
+    for line in sections.next().unwrap().split('\n') {
+        map.insert(line[0..3].to_string(), NextStage{left:line[7..10]. to_owned(), right: line[12..15].to_owned()});
+    }
+    (steps, map)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const DATA: &str = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+    #[test]
+    fn test_parser() {
+        let data =  "RL\n\nAAA = (BBB, CCC)";
+        assert_eq!(parse_data(data), 
+            ("RL".to_owned(), 
+            HashMap::from([("AAA".to_string(), NextStage{left: "BBB".to_string(), right: "CCC". to_string()})])));
+    }
+
+    #[test]
+    fn test_score_part1() {
+        let (directions, map) = parse_data(DATA);
+        assert_eq!(follow_directions(&directions, &map, "AAA"), 2);
+        let (directions, map)= parse_data("LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)");
+        assert_eq!(follow_directions(&directions, &map, "AAA"), 6);
+    }
+
+    #[test]
+    fn test_part2() {
+        let data = "LR\n\n11A = (11B, XXX)\n11B = (XXX, 11Z)\n11Z = (11B, XXX)\n22A = (22B, XXX)\n22B = (22C, 22C)\n22C = (22Z, 22Z)\n22Z = (22B, 22B)\nXXX = (XXX, XXX)";
+        let (directions, map) = parse_data(data);
+        assert_eq!(direct_ghosts(&directions, &map), 6);
+
+    }
+}
